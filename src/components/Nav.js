888bf1b75744e4c23c37d7ba94d9a59de0aeaa40
Juan <jmanuel.dealava@gmail.com>
2023-03-24 01:32:12 -0300

    styling the UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,13 @@ import logo from '../Logo.svg'
 function Nav() {
     return (
         <nav>
-            <img src={logo} alt='carlos'/>
+            <img className='logo' src={logo} alt='carlos'/>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Menu</a></li>
                 <li><a href="/">Reservations</a></li>
-                <li><a href="/">Oreder-online</a></li>
+                <li><a href="/">Order Online</a></li>
                 <li><a href="/">Login</a></li>
             </ul>
         </nav>
